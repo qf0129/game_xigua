23e91183b0e65cf1adb35677fec9b169c2d66993
qf0129 <qf0129@qq.com>
2021-02-17 23:21:09 +0800

    update

--- src/base/music.js
+++ src/base/music.js
@@ -4,23 +4,14 @@ export default class Music {
   constructor() {
     if (instance) return instance
     instance = this
-
     // this.bgmAudio = new Audio()
     // this.bgmAudio.loop = true
     // this.bgmAudio.src = 'audio/bgm.mp3'
-
     this.boomAudio = new Audio()
-    this.boomAudio.src = 'audio/fit2.mp3'
-
+    this.boomAudio.src = 'audio/audio6.mp3'
     this.mergeAudio = new Audio()
-    this.mergeAudio.src = 'audio/fit.mp3'
-
-    // this.playBgm()
+    this.mergeAudio.src = 'audio/audio7.mp3'
   }
-
-  // playBgm() {
-  //   this.bgmAudio.play()
-  // }
 
   playMerge() {
     this.mergeAudio.currentTime = 0
